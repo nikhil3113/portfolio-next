c735653ec77ebc76e3f50b6f30dc100f8d85eae4
nikhil3113 <chavann717@gmail.com>
2025-04-24 21:49:11 +0530

    updated favicon

--- components/ui/resizable-navbar.tsx
+++ components/ui/resizable-navbar.tsx
@@ -8,6 +8,7 @@ import {
   useScroll,
   useMotionValueEvent,
 } from "motion/react";
+import Image from "next/image";
 // import Image from "next/image";
 
 import React, { useRef, useState } from "react";
@@ -69,7 +70,6 @@ export const Navbar = ({ children, className }: NavbarProps) => {
   return (
     <motion.div
       ref={ref}
-      // IMPORTANT: Change this to class of `fixed` if you want the navbar to be fixed
       className={cn("sticky inset-x-0 top-0 z-40 w-full", className)}
     >
       {React.Children.map(children, (child) =>
@@ -245,13 +245,13 @@ export const NavbarLogo = () => {
       href="/"
       className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black"
     >
-      {/* <Image
-        src="https://assets.aceternity.com/logo-dark.png"
+      <Image
+        src="/images/logo.png"
         alt="logo"
         width={30}
         height={30}
-      /> */}
-      <span className="font-medium text-black dark:text-white">Portfolio</span>
+      />
+      <span className="font-medium text-black dark:text-white">Nikhil</span>
     </a>
   );
 };
